6eb2809485f3cbd5606e3cfc563df7a1580fad58
KingzMen <you@example.com>
2019-11-28 05:12:14 +0100

    Second commit after creating bookmarks and updating event listeners in js

--- js/index.js
+++ js/index.js
@@ -17,3 +17,26 @@ input.addEventListener("focusin", floatInput);
 input.addEventListener("focusout", closeOverlay);
 
 overlay.addEventListener("click", closeOverlay);
+
+///====================================
+const bookmarksList = document.querySelector(".bookmarks-list");
+const bookmarkForm = document.querySelector(".bookmark-form");
+const bookmarkInput = document.querySelector("input[type=text]");
+const button = document.querySelector(".btn");
+
+function createBookmark(e) {
+  e.preventDefault(); //Stops the default HTML form refresh
+
+  const title = bookmarkInput.value;
+  const bookmark = document.createElement("a");
+  bookmark.className = "bookmark";
+  bookmark.innerText = title;
+  bookmark.href = "#";
+  bookmark.target = "_blank";
+  bookmarksList.appendChild(bookmark);
+
+  bookmarkForm.reset(); ///to clear input field after form submission
+}
+
+bookmarkForm.addEventListener("submit", createBookmark);
+button.addEventListener("click", createBookmark);
